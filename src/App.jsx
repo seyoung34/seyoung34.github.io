53b157ed319b1be9428763a40a717617e9eb4b8a
seyoung34 <jupi0304@naver.com>
2025-09-17 02:56:37 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-// import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <h1>해은아 안녕</h1>
+        <h1>🌸 해은아 안녕 🌸</h1>
+        <h1>✨ 우하하하하하ㅏ하하 🌈</h1>
+        <p>🐰 🐥 🐻</p>
       </div>
     </>
   )
 }
+
 
 export default App
